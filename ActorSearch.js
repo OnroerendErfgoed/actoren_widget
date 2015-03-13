d951db615198ea4b90257dc21386b1bd12d32fb1
Tinne Cahy <tinne.cahy@geosolutions.be>
2015-03-13 16:35:06 +0100

    use ondemandgrid

--- ActorSearch.js
+++ ActorSearch.js
@@ -2,12 +2,14 @@ define([
   'dojo/text!./templates/ActorSearch.html',
   'dojo/_base/declare',
   'dojo/_base/lang',
+  'dojo/request',
   'dijit/_WidgetBase',
   'dijit/_TemplatedMixin',
   'dijit/_WidgetsInTemplateMixin',
-  "dgrid/extensions/Pagination",
   "dojo/store/Observable",
   "dojo/store/JsonRest",
+  'dgrid/Keyboard',
+  'dgrid/extensions/DijitRegistry',
   'dgrid/OnDemandGrid',
   'dgrid/Selection',
   'dgrid/selector'
@@ -15,44 +17,43 @@ define([
   template,
   declare,
   lang,
+  request,
   _WidgetBase,
   _TemplatedMixin,
   _WidgetsInTemplateMixin,
-  Pagination,
   Observable,
   JsonRest,
+  Keyboard,
+  DijitRegistry,
   OnDemandGrid,
   Selection,
   selector
 ) {
   return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
-    templateString: template,
-    baseClass: 'actor-search',
-    widgetsInTemplate: true,
+	templateString: template,
+	baseClass: 'actor-search',
+	widgetsInTemplate: true,
 	actorStore: null,
 	baseUrl: null,
+	_previousSearchValue:'',
 
-    postCreate: function() {
-      console.log('..ActorSearch::postCreate', arguments);
-      this.inherited(arguments);
+	postCreate: function() {
+	  console.log('..ActorSearch::postCreate', arguments);
+	  this.inherited(arguments);
 	  this.actorStore = new Observable(new JsonRest({
 		target: this.baseUrl + '/actoren/wij',
-		//target: this.baseUrl + '/actoren',
 		sortParam: 'sort',
 		idProperty:'id'
 	  }));
 	},
 
-    startup: function () {
+	startup: function () {
 	  console.log('..ActorSearch::startup', arguments);
-      this.inherited(arguments);
+	  this.inherited(arguments);
 	  this._createGrid();
-	  this._grid.on( 'dgrid-refresh-complete' , function (event) {
-		console.log(event);
-	  });
 
-    },
+	},
 
 	_createGrid: function () {
 	  var columns = {
@@ -71,40 +72,48 @@ define([
 		}
 	  };
 
-      this._grid = new (declare([OnDemandGrid, Selection, Pagination]))({
-        store: this.actorStore,
-        columns: columns,
-		pagingLinks: 1,
-		pagingTextBox: true,
-		firstLastArrows: true,
-        sort: [
-          { attribute: 'id' }
-        ],
+	  this._grid = new (declare([OnDemandGrid, Selection, Keyboard, DijitRegistry]))({
+		store: this.actorStore,
+		columns: columns,
+		sort: [
+		  { attribute: 'naam' }
+		],
 		loadingMessage: 'laden...',
-        noDataMessage: 'geen resultaten beschikbaar'
-      }, this.gridNode);
+		noDataMessage: 'geen resultaten beschikbaar'
+	  }, this.gridNode);
 
+	  this._grid.on(".dgrid-row:click", lang.hitch(this, function(evt){
+		var id = this._grid.row(event).id;
+		var item = request(this.baseUrl + '/actoren/' + id, {
+		  headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		  }
+		}).then(function(actor){
+		  console.log(actor);
+		});
 
-	  return this._grid;
+	  }));
+
 	},
 
-    _filterGrid: function (evt) {
-      var newValue = evt.target.value;
-      if (this._timeoutId) {
-        clearTimeout(this._timeoutId);
-        this._timeoutId = null;
-      }
-      this._timeoutId = setTimeout(lang.hitch(this, function() {
-		if (newValue === '') {
-		  this._grid.setQuery({});
-		  this._grid.refresh()
+	_filterGrid: function (evt) {
+	  var newValue = evt.target.value;
+	  if (this._timeoutId) {
+		clearTimeout(this._timeoutId);
+		this._timeoutId = null;
+	  }
+	  this._timeoutId = setTimeout(lang.hitch(this, function () {
+		if (newValue != this._previousSearchValue) {
+		  this._previousSearchValue = newValue;
+		  if (newValue === '') {
+			this._grid.setQuery({});
+		  }
+		  else {
+			this._grid.set("query", {"omschrijving": newValue});
+		  }
 		}
-		else {
-		  this._grid.set("query", {"query":newValue});
-		  this._grid.refresh();
-		}
-      }, 30));
-    }
-
+	  }, 30));
+	}
   });
 });
