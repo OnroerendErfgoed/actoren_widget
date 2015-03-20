cdec77eed9db9dcaf775f9fe96d89d7675c42ec7
Vanderhaegen Cedrik <cedrik.vanderhaegen@geosolutions.be>
2015-03-20 09:37:40 +0100

    prevent child remove / create on each change

--- ActorWidget.js
+++ ActorWidget.js
@@ -12,27 +12,27 @@ define([
   './ActorCreate/ActorCreate',
   'dijit/layout/StackContainer'
 ], function (
-    template,
-    declare,
-    _WidgetBase,
-    _TemplatedMixin,
-    _WidgetsInTemplateMixin,
-	ActorController,
-    ActorSearch,
-	ActorDetail,
-	ActorEdit,
-	ActorAdvancedSearch,
-	ActorCreate
+  template,
+  declare,
+  _WidgetBase,
+  _TemplatedMixin,
+  _WidgetsInTemplateMixin,
+  ActorController,
+  ActorSearch,
+  ActorDetail,
+  ActorEdit,
+  ActorAdvancedSearch,
+  ActorCreate
 ) {
   return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
     templateString: template,
     baseClass: 'actor-widget',
     widgetsInTemplate: true,
-	baseUrl: null,
-	actorController: null,
-	erfgoed_id: null,
-	crabHost: null,
+    baseUrl: null,
+    actorController: null,
+    erfgoed_id: null,
+    crabHost: null,
 
     _actorSearch: null,
 
@@ -40,65 +40,40 @@ define([
       this.inherited(arguments);
       console.log('ActorWidget::postCreate', arguments);
 
-	  this.actorController = new ActorController({baseUrl: this.baseUrl});
-	  this._setupLayout();
+      this.actorController = new ActorController({baseUrl: this.baseUrl});
+      this._setupLayout();
 
-	  this.on('send.actor', function(evt){
-		console.log('send.actor');
-		console.log(evt.actor);
-	  });
+      this.on('send.actor', function(evt){
+        console.log('send.actor');
+        console.log(evt.actor);
+      });
     },
 
     startup: function () {
       this.inherited(arguments);
       console.log('ActorWidget::startup', arguments);
-	  this.showSearch();
+      this.showSearch();
     },
 
     showSearch: function () {
-      this.actorStackContainer.addChild(this._actorSearch);
-      this.actorStackContainer.removeChild(this._actorDetail);
-      this.actorStackContainer.removeChild(this._actorEdit);
-      this.actorStackContainer.removeChild(this._actorAdvancedSearch);
-      this.actorStackContainer.removeChild(this._actorCreate);
       this.actorStackContainer.selectChild(this._actorSearch);
     },
 
     showDetail: function (actor) {
-	  this.actorStackContainer.addChild(this._actorDetail);
-	  this._actorDetail.setActor(actor);
-	  this.actorStackContainer.removeChild(this._actorSearch);
-	  this.actorStackContainer.removeChild(this._actorEdit);
-	  this.actorStackContainer.removeChild(this._actorAdvancedSearch);
-	  this.actorStackContainer.removeChild(this._actorCreate);
+      this._actorDetail.setActor(actor);
       this.actorStackContainer.selectChild(this._actorDetail);
     },
 
     showEdit: function (actor) {
-	  this.actorStackContainer.addChild(this._actorEdit);
-	  this._actorEdit.setActor(actor);
-	  this.actorStackContainer.removeChild(this._actorSearch);
-	  this.actorStackContainer.removeChild(this._actorDetail);
-	  this.actorStackContainer.removeChild(this._actorAdvancedSearch);
-	  this.actorStackContainer.removeChild(this._actorCreate);
+      this._actorEdit.setActor(actor);
       this.actorStackContainer.selectChild(this._actorEdit);
     },
 
     showAdvancedSearch: function () {
-	  this.actorStackContainer.addChild(this._actorAdvancedSearch);
-	  this.actorStackContainer.removeChild(this._actorSearch);
-	  this.actorStackContainer.removeChild(this._actorDetail);
-	  this.actorStackContainer.removeChild(this._actorEdit);
-	  this.actorStackContainer.removeChild(this._actorCreate);
       this.actorStackContainer.selectChild(this._actorAdvancedSearch);
     },
 
     showCreate: function () {
-	  this.actorStackContainer.addChild(this._actorCreate);
-	  this.actorStackContainer.removeChild(this._actorSearch);
-	  this.actorStackContainer.removeChild(this._actorDetail);
-	  this.actorStackContainer.removeChild(this._actorEdit);
-	  this.actorStackContainer.removeChild(this._actorAdvancedSearch);
       this.actorStackContainer.selectChild(this._actorCreate);
     },
 
@@ -108,11 +83,18 @@ define([
       this._actorEdit = new ActorEdit({actorWidget: this});
       this._actorAdvancedSearch = new ActorAdvancedSearch({actorWidget: this});
       this._actorCreate = new ActorCreate({actorWidget: this});
+
+      this.actorStackContainer.addChild(this._actorSearch);
+      this.actorStackContainer.addChild(this._actorDetail);
+      this.actorStackContainer.addChild(this._actorEdit);
+      this.actorStackContainer.addChild(this._actorAdvancedSearch);
+      this.actorStackContainer.addChild(this._actorCreate);
+
     },
 
-	emitActor: function(actor) {
-	 this.emit('send.actor', {actor: actor})
-	}
+    emitActor: function(actor) {
+      this.emit('send.actor', {actor: actor});
+    }
 
   });
 });
