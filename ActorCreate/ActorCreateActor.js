a5045100e80468278be5f482df84b5cd37946d01
Vanderhaegen Cedrik <cedrik.vanderhaegen@geosolutions.be>
2015-03-20 10:41:01 +0100

    add country list to phone input

--- ActorCreate/ActorCreateActor.js
+++ ActorCreate/ActorCreateActor.js
@@ -3,19 +3,20 @@ define([
   'dojo/_base/declare',
   'dijit/_WidgetBase',
   'dijit/_TemplatedMixin',
-  'dijit/_WidgetsInTemplateMixin'
+  'dojo/store/Memory',
+  'dijit/form/ComboBox'
 ], function(
   template,
   declare,
   _WidgetBase,
   _TemplatedMixin,
-  _WidgetsInTemplateMixin
+  Memory,
+  ComboBox
 ) {
-  return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+  return declare([_WidgetBase, _TemplatedMixin], {
 
 	templateString: template,
 	baseClass: 'actor-widget',
-	widgetsInTemplate: true,
 	actor: null,
 	createWidget: null,
 
@@ -27,6 +28,30 @@ define([
 
 	startup: function () {
 	  console.log('...ActorCreateActor::startup', arguments);
+    this.inherited(arguments);
+    this._setTelefoonLandcodes();
+	},
+
+	_setTelefoonLandcodes: function() {
+    var countryCodeStore = new Memory({
+      data: [
+        {name:"België (+32)", id:"32"},
+        {name:"Duitsland (+49)", id:"49"},
+        {name:"Frankrijk (+33)", id:"33"},
+        {name:"Groot-Brittannië (+44)", id:"44"},
+        {name:"Nederland (+31)", id:"31"},
+        {name:"Luxemburg (+352)", id:"352"}
+      ]
+    });
+
+    new ComboBox({
+      store: countryCodeStore,
+      value: "België (+32)",
+      hasDownArrow: false,
+      searchAttr: "name",
+      autoComplete: false,
+      required: false
+    }, this.telefoonLandcode);
 	},
 
 	_openSearch: function() {
