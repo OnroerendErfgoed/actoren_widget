64b215e3dc67803cf2b510a78d1f573385950778
Tinne Cahy <tinne.cahy@geosolutions.be>
2015-03-23 15:41:24 +0100

    crabwidget in ActorenSearchVKBP

--- ActorAdvancedSearch/ActorAdvSearchVKBP.js
+++ ActorAdvancedSearch/ActorAdvSearchVKBP.js
@@ -1,67 +1,75 @@
 define([
-  'dojo/text!./../templates/ActorAdvancedSearch/ActorAdvSearchVKBP.html',
-  'dojo/_base/declare',
-  'dojo/_base/lang',
-  'dijit/_WidgetBase',
-  'dijit/_TemplatedMixin',
-  'dijit/_WidgetsInTemplateMixin'
+	'dojo/text!./../templates/ActorAdvancedSearch/ActorAdvSearchVKBP.html',
+	'dojo/_base/declare',
+	'dojo/_base/lang',
+	'dijit/_WidgetBase',
+	'dijit/_TemplatedMixin',
+	'dijit/_WidgetsInTemplateMixin',
+	'../CrabWidget'
 ], function(
-  template,
-  declare,
-  lang,
-  _WidgetBase,
-  _TemplatedMixin,
-  _WidgetsInTemplateMixin
+	template,
+	declare,
+	lang,
+	_WidgetBase,
+	_TemplatedMixin,
+	_WidgetsInTemplateMixin,
+	CrabWidget
 ) {
-  return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+	return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
-	templateString: template,
-	baseClass: 'actor-widget',
-	widgetsInTemplate: true,
-	searchWidget: null,
+		templateString: template,
+		baseClass: 'actor-widget',
+		widgetsInTemplate: true,
+		searchWidget: null,
 
-	postCreate: function() {
-	  console.log('...ActorAdvSearchVKBO::postCreate', arguments);
-	  this.inherited(arguments);
-	},
+		postCreate: function() {
+			console.log('...ActorAdvSearchVKBO::postCreate', arguments);
+			this.inherited(arguments);
+		},
 
-	startup: function () {
-	  console.log('...ActorAdvSearchVKBO::startup', arguments);
-	  this.inherited(arguments);
-	},
+		startup: function () {
+			console.log('...ActorAdvSearchVKBO::startup', arguments);
+			this.inherited(arguments);
+			this._setCrabWidget();
+		},
 
-	_findVKBP: function() {
-	  var query = this._getSearchParams();
-	  this._filterGrid(query);
-	  this._showSearch();
-	},
+		_setCrabWidget: function() {
+			this._crabWidget = new CrabWidget({crabController: this.actorWidget.crabController}, this.crabWidget);
+		},
 
-	_getSearchParams: function() {
-	  var query = {'type': 1};
-	  var searchParams = [
-		'naam',
-		'straat',
-		'nummer',
-		'postbus',
-	  	'gemeente',
-		'land',
-		'persid',
-		'rrn'
-	  ];
-	  searchParams.forEach(lang.hitch(this, function(param) {
-		if (this[param].value) {
-		  query[param] = this[param].value;
+		_findVKBP: function() {
+			var query = this._getSearchParams();
+			this._filterGrid(query);
+			this._showSearch();
+		},
+
+		_getSearchParams: function() {
+			var query = {'type': 1};
+			var searchParams = [
+				'naam',
+				'persid',
+				'rrn'
+			];
+			searchParams.forEach(lang.hitch(this, function(param) {
+				if (this[param].value) {
+					query[param] = this[param].value;
+				}
+			}));
+			var crabParams = this._crabWidget.getValues();
+			Object.keys(crabParams).forEach(function(param){
+				if(crabParams[param]) {
+					query[param] = crabParams[param];
+				}
+			});
+			return query;
+		},
+
+		_filterGrid: function (query) {
+			this.actorWidget._actorSearch.AdvSearchFilterGrid(query);
+		},
+
+		_openSearch: function() {
+			this.actorWidget.showSearch();
 		}
-	  }));
-	  return query;
-	},
-
-	_filterGrid: function (query) {
-	  this.actorWidget._actorSearch.AdvSearchFilterGrid(query);
-	},
-
-	_openSearch: function() {
-	    this.actorWidget.showSearch();
-	}
-  });
+	});
 });
