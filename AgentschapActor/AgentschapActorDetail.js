4973e6e1bff1979024e799ccb6c58075d225b882
Maarten Taeymans <maarten.taeymans@geosolutions.be>
2015-03-25 11:41:20 +0100

    styling agentschapactorwidget

--- AgentschapActor/AgentschapActorDetail.js
+++ AgentschapActor/AgentschapActorDetail.js
@@ -13,60 +13,51 @@ define([
 ) {
   return declare([_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
 
-	templateString: template,
-	baseClass: 'actor-widget',
-	widgetsInTemplate: true,
-	actor: null,
-	actorWidget: null,
+    templateString: template,
+    baseClass: 'actor-widget',
+    widgetsInTemplate: true,
+    actor: null,
+    actorWidget: null,
 
 
-	postCreate: function() {
-	  console.log('..ActorDetail::postCreate', arguments);
-	  this.inherited(arguments);
-	},
+    postCreate: function() {
+      console.log('..ActorDetail::postCreate', arguments);
+      this.inherited(arguments);
+    },
 
-	startup: function () {
-	  console.log('..ActorDetail::startup', arguments);
-	  this.inherited(arguments);
-	},
+    startup: function () {
+      console.log('..ActorDetail::startup', arguments);
+      this.inherited(arguments);
+    },
 
-	setActor: function(actor) {
-	  this.naam.value = actor.naam;
-	  this.voornaam.value = actor.voornaam;
-	  var email_adres = null;
-	  actor.emails.forEach(function(email) {
-		if (email.type.naam == "werk"){
-		  email_adres = email.email;
-		}
-	  });
-	  if (!email_adres && actor.emails.length > 0) {
-		email_adres = actor.emails[0].email;
-	  }
-	  this.email.value  = email_adres;
-	  var telefoon_nummer = null;
-	  var telefoon_landcode = null;
-	  actor.telefoons.forEach(function(telefoon) {
-		if (telefoon.type.naam == "werk"){
-		  telefoon_nummer = telefoon.nummer;
-		  telefoon_landcode = telefoon.landcode ? telefoon.landcode : null;
-		}
-	  });
-	  if (!telefoon_nummer && actor.telefoons.length > 0) {
-		  telefoon_nummer = actor.telefoons[0].nummer;
-		  telefoon_landcode = telefoon.landcode ? telefoon.landcode : null;
-	  }
-	  this.telefoon.value  = telefoon_nummer;
-	  this.telefoonLandcode.value  = telefoon_landcode;
-	  this.straat.value  = actor.adres ? actor.adres.straat : null;
-	  this.nummer.value  = actor.adres ? actor.adres.huisnummer : null;
-	  this.postcode.value  = actor.adres ? actor.adres.postcode : null;
-	  this.gemeente.value  = actor.adres ? actor.adres.gemeente : null;
-	  this.land.value  = actor.adres ? actor.adres.land : null;
-	  this.actortype.value  = actor.type.naam;
-	  this.actor = actor;
-	},
-	_openSearch: function() {
-	  this.actorWidget.showSearch();
-	}
+    setActor: function(actor) {
+      this.naam.value = actor.naam;
+      this.voornaam.value = actor.voornaam;
+      var email = actor.emails.filter(function(email){
+        return email.type.naam == "werk";
+      });
+      if (!email.length && actor.emails.length > 0) {
+        email = actor.emails.slice(0, 1);
+      }
+      this.email.value  = email.length ? email[0].email : null;
+      var telefoon = actor.telefoons.filter(function(telefoon) {
+        return telefoon.type.naam == "werk"
+      });
+      if (!telefoon.length && actor.telefoons.length > 0) {
+        telefoon = actor.telefoons.slice(0, 1);
+      }
+      this.telefoon.value  = telefoon.length ? telefoon[0].nummer : null;
+      this.telefoonLandcode.value  = telefoon.length ? telefoon[0].landcode ? telefoon[0].landcode : null : null;
+      this.straat.value  = actor.adres ? actor.adres.straat : null;
+      this.nummer.value  = actor.adres ? actor.adres.huisnummer : null;
+      this.postcode.value  = actor.adres ? actor.adres.postcode : null;
+      this.gemeente.value  = actor.adres ? actor.adres.gemeente : null;
+      this.land.value  = actor.adres ? actor.adres.land : null;
+      this.actortype.value  = actor.type.naam;
+      this.actor = actor;
+    },
+    _openSearch: function() {
+      this.actorWidget.showSearch();
+    }
   });
 });
