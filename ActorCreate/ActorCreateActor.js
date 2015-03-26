992bfda92acc96a24315c4346a0d6cdacdaca204
Tinne Cahy <tinne.cahy@geosolutions.be>
2015-03-26 11:49:50 +0100

    actoren aanmaken get values

--- ActorCreate/ActorCreateActor.js
+++ ActorCreate/ActorCreateActor.js
@@ -1,70 +1,71 @@
 define([
-  'dojo/text!./../templates/ActorCreate/ActorCreateActor.html',
-  'dojo/_base/declare',
-  'dijit/_WidgetBase',
-  'dijit/_TemplatedMixin',
-  'dojo/store/Memory',
-  'dijit/form/ComboBox',
+	'dojo/text!./../templates/ActorCreate/ActorCreateActor.html',
+	'dojo/_base/declare',
+	'dijit/_WidgetBase',
+	'dijit/_TemplatedMixin',
+	'dojo/store/Memory',
+	'dijit/form/ComboBox',
 	'../CrabWidget'
 ], function(
-  template,
-  declare,
-  _WidgetBase,
-  _TemplatedMixin,
-  Memory,
-  ComboBox,
+	template,
+	declare,
+	_WidgetBase,
+	_TemplatedMixin,
+	Memory,
+	ComboBox,
 	CrabWidget
 ) {
-  return declare([_WidgetBase, _TemplatedMixin], {
+	return declare([_WidgetBase, _TemplatedMixin], {
 
-    templateString: template,
-    baseClass: 'actor-widget',
-    actor: null,
+		templateString: template,
+		baseClass: 'actor-widget',
+		actor: null,
 		actorWidget: null,
 		actorAdvancedSearch : null,
-    _telefoonLandcodeSelect: null,
+		_telefoonLandcodeSelect: null,
 
 		_actorTelefoons: {},
 		_actorEmails: {},
+		_actorUrls: {},
 
 
-    postCreate: function() {
-      console.log('...ActorCreateActor::postCreate', arguments);
-      this.inherited(arguments);
-    },
+		postCreate: function() {
+			console.log('...ActorCreateActor::postCreate', arguments);
+			this.inherited(arguments);
+		},
 
-    startup: function () {
-      console.log('...ActorCreateActor::startup', arguments);
-      this.inherited(arguments);
-      this._setTelefoonLandcodes();
+		startup: function () {
+			console.log('...ActorCreateActor::startup', arguments);
+			this.inherited(arguments);
+			this._setTelefoonLandcodes();
 			this._setCrabWidget();
 		},
 
-    _setTelefoonLandcodes: function() {
-      var countryCodeStore = new Memory({
-        data: [
-          {name:"+32",  id:"32",  label:"<span class='flag be'>België (+32)</span>"},
-          {name:"+49",  id:"49",  label:"<span class='flag de'>Duitsland (+49)</span>"},
-          {name:"+33",  id:"33",  label:"<span class='flag fr'>Frankrijk (+33)</span>"},
-          {name:"+44",  id:"44",  label:"<span class='flag gb'>Groot-Brittannië (+44)</span>"},
-          {name:"+31",  id:"31",  label:"<span class='flag nl'>Nederland (+31)</span>"},
-          {name:"+352", id:"352", label:"<span class='flag lu'>Luxemburg (+352)</span>"}
-        ]
-      });
+		_setTelefoonLandcodes: function() {
+			var countryCodeStore = new Memory({
+				data: [
+					{name:"+32",  id:"32",  label:"<span class='flag be'>België (+32)</span>"},
+					{name:"+49",  id:"49",  label:"<span class='flag de'>Duitsland (+49)</span>"},
+					{name:"+33",  id:"33",  label:"<span class='flag fr'>Frankrijk (+33)</span>"},
+					{name:"+44",  id:"44",  label:"<span class='flag gb'>Groot-Brittannië (+44)</span>"},
+					{name:"+31",  id:"31",  label:"<span class='flag nl'>Nederland (+31)</span>"},
+					{name:"+352", id:"352", label:"<span class='flag lu'>Luxemburg (+352)</span>"}
+				]
+			});
 
-      this._telefoonLandcodeSelect = new ComboBox({
-        store: countryCodeStore,
-        value: "+32",
-        hasDownArrow: true,
-        searchAttr: "name",
-        autoComplete: false,
-        required: false,
-        class: "combo-dropdown",
-        style: "width: 20%; float: left; padding-left: 10px;",
-        labelAttr: "label",
-        labelType: "html"
-      }, this.telefoonLandcode);
-    },
+			this._telefoonLandcodeSelect = new ComboBox({
+				store: countryCodeStore,
+				value: "+32",
+				hasDownArrow: true,
+				searchAttr: "name",
+				autoComplete: false,
+				required: false,
+				class: "combo-dropdown",
+				style: "width: 20%; float: left; padding-left: 10px;",
+				labelAttr: "label",
+				labelType: "html"
+			}, this.telefoonLandcode);
+		},
 
 		_watchTelefoonTypes: function () {
 			this.telefoon.value = this._actorTelefoons[this.telefoontypes.selectedOptions[0].value] ?
@@ -75,8 +76,8 @@ define([
 
 		_watchTelefoonInput: function() {
 			this._actorTelefoons[this.telefoontypes.selectedOptions[0].value] = {
-				'nummer': this.telefoon.value,
-				'landcode': this._telefoonLandcodeSelect.get('value')
+				nummer: this.telefoon.value,
+				landcode: this._telefoonLandcodeSelect.get('value')
 			}
 		},
 
@@ -87,7 +88,18 @@ define([
 
 		_watchEmailInput: function() {
 			this._actorEmails[this.emailtypes.selectedOptions[0].value] = {
-				'email': this.email.value
+				email: this.email.value
+			}
+		},
+
+		_watchUrlTypes: function () {
+			this.url.value = this._actorUrls[this.urltypes.selectedOptions[0].value] ?
+				this._actorUrls[this.urltypes.selectedOptions[0].value].url : null;
+		},
+
+		_watchUrlInput: function() {
+			this._actorUrls[this.urltypes.selectedOptions[0].value] = {
+				url: this.url.value
 			}
 		},
 
@@ -95,14 +107,14 @@ define([
 			this._crabWidget = new CrabWidget({crabController: this.actorWidget.crabController}, this.crabWidget);
 		},
 
-    _openSearch: function() {
-      this.actorAdvancedSearch._showSearch();
-    },
+		_openSearch: function() {
+			this.actorAdvancedSearch._showSearch();
+		},
 
-    _showActorSearch: function() {
-      this.actorAdvancedSearch._showActorSearch();
+		_showActorSearch: function() {
+			this.actorAdvancedSearch._showActorSearch();
 			this._reset();
-    },
+		},
 
 		_reset: function(){
 			this.naam.value = "";
@@ -113,11 +125,15 @@ define([
 
 		_save: function() {
 			var actorNew = {};
-			// naam
 			actorNew['naam'] = this.naam.value;
-			// voornaam
 			actorNew['voornaam'] = this.voornaam.value;
-			// telefoon
+			actorNew['rrn'] = this.rrn.value;
+			actorNew['kbo'] = this.kbo.value;
+			actorNew['actortype'] = {
+				type: {
+					id: this.type.value
+				}
+			};
 			actorNew['telefoons'] =[];
 			for (var telefoontype in this._actorTelefoons){
 				actorNew['telefoons'].push(
@@ -130,7 +146,6 @@ define([
 					}
 				)
 			}
-			// email
 			actorNew['emails'] =[];
 			for (var emailtype in this._actorEmails){
 				actorNew['emails'].push(
@@ -142,15 +157,21 @@ define([
 					}
 				)
 			}
-			// url
+			actorNew['urls'] =[];
+			for (var urltype in this._actorUrls){
+				actorNew['urls'].push(
+					{
+						type: {
+							id: urltype
+						},
+						email: this._actorUrls[urltype].url
+					}
+				)
+			}
 			// adres
 			// console.log(this._crabWidget.getInput());
-			// actortype
-			// rrn
-			// persid
-			// kbo
 
 			console.log(actorNew);
 		}
-  });
+	});
 });
