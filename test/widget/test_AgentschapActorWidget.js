41c7e9c12cf49123d7468be8416d774283cfe48a
Vanderhaegen Cedrik <cedrik.vanderhaegen@geosolutions.be>
2015-03-31 20:26:36 +0200

    move JsonRestCors file to test folder

--- test/widget/test_AgentschapActorWidget.js
+++ test/widget/test_AgentschapActorWidget.js
@@ -1,10 +1,41 @@
 require([
   'actorwidget/AgentschapActor/AgentschapActorWidget',
+	'dojo/store/Observable',
+	'actorwidget/test/util/JsonRestCors',
   'dojo/domReady!'
-], function (AgentschapActorWidget) {
+], function (
+  AgentschapActorWidget,
+	Observable,
+	JsonRestCors
+) {
+
+  var baseUrl= "http://localhost:6543";
+
+  var actorWijStore = new Observable(new JsonRestCors({
+    target: baseUrl + '/actoren/wij/',
+    sortParam: 'sort',
+    idProperty: 'id',
+    withCredentials: true,
+    headers: {
+      "X-Requested-With": "",
+      "Content-Type": "application/json"
+    }
+  }));
+
+  var actorStore = new Observable(new JsonRestCors({
+    target: baseUrl + '/actoren/',
+    sortParam: 'sort',
+    idProperty: 'id',
+    withCredentials: true,
+    headers: {
+      "X-Requested-With": "",
+      "Content-Type": "application/json"
+    }
+  }));
 
   var actorWidget = new AgentschapActorWidget({
-	baseUrl: "http://localhost:6543"
+    actorWijStore: actorWijStore,
+    actorStore: actorStore
   }, 'widgetNode');
   actorWidget.startup();
 
