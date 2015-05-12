e2164eb68fbcd258395fcb7a4991e51f7a9dfb4f
julien <julien.devos@geosolutions.be>
2015-05-12 15:41:34 +0200

    documentations and simplifications

--- Actor/ActorSelection.js
+++ Actor/ActorSelection.js
@@ -1,3 +1,8 @@
+/**
+ * Extension to ActorWidget that allows to track the state of the selection on the result grid of ActorSearch by relaying its
+ * Selection event with an event of its own 'select.actors'
+ * @module Actor/ActorSelection
+ */
 define([
     'dojo/_base/declare',
     'dojo/_base/lang',
@@ -5,25 +10,22 @@ define([
     'dojo/Evented'
 ], function (declare, lang, on, Evented) {
     return declare(Evented, {
-
-        postMixInProperties: function () {
-            this.inherited(arguments);
-        },
-
-        buildRendering: function () {
-            this.inherited(arguments);
-        },
-
         postCreate: function () {
+            console.log('..ActorSelection::postCreate', arguments);
             this.inherited(arguments);
         },
 
         startup: function () {
+            console.log('..ActorSelection::startup', arguments);
             this.inherited(arguments);
+
+            //verifies necessary components are present
             if (!('_actorSearch' in this) || !('_grid' in this._actorSearch)) {
                 console.error('Could not initiate actor selection on actor widget');
                 return;
             }
+
+            //relays selection event
             this.own(on(this._actorSearch._grid, "dgrid-select", lang.hitch(this, function (event) {
                 var selected = event.rows.map(function (row) {
                     return row.data;
