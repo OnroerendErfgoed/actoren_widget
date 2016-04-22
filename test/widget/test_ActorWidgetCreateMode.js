fc23771d255171e9416e1ef284343ea930c96466
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-04-22 14:09:31 +0200

    add test page for create view startup

---
+++ test/widget/test_ActorWidgetCreateMode.js
@@ -0,0 +1,51 @@
+require([
+	'actorwidget/widgets/ActorWidget',
+	'dojo/store/Observable',
+	'dojo/store/JsonRest',
+	'dojo/domReady!'
+], function (
+	ActorWidget,
+	Observable,
+	JsonRest
+) {
+	var baseUrl= 'https://dev-actoren.onroerenderfgoed.be';
+	var ssoToken = 'AQIC5wM2LY4SfcxBiA0_fpFhmsmyCzZ5fa2CXq8TmNS-3ow.*AAJTSQACMDIAAlNLABEyNjUwMjMxNjk0MjkyNDM2OQACUzEAAjAx*';
+
+	var actorWijStore = new Observable(new JsonRest({
+		target: baseUrl + '/actoren/wij/',
+		sortParam: 'sort',
+		idProperty: 'id',
+		headers: {
+			'X-Requested-With': '',
+			'Content-Type': 'application/json',
+			'OpenAmSSOID': ssoToken
+		}
+	}));
+
+	var actorStore = new Observable(new JsonRest({
+		target: baseUrl + '/actoren/',
+		sortParam: 'sort',
+		idProperty: 'id',
+		headers: {
+			'X-Requested-With': '',
+			'Content-Type': 'application/json',
+			'OpenAmSSOID': ssoToken
+		}
+	}));
+
+	var actorWidget = new ActorWidget({
+		actorWijStore: actorWijStore,
+		actorStore: actorStore,
+    canCreateActor: true,
+    canEditActor: true,
+		ssoToken: ssoToken,
+		actorCategories: {
+			actoren: true,
+			vkbo: false,
+			vkbp: false
+		},
+		crabHost: baseUrl
+	}, 'widgetNode');
+	actorWidget.startup();
+
+});
