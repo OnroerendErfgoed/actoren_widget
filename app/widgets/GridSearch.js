8e0af9f2b50d5db43a2080ae8e3449fd807d3aaa
Emrys Roef <emrys.roef@geosolutions.be>
2023-09-22 11:46:02 +0200

    #219 fix double call

--- app/widgets/GridSearch.js
+++ app/widgets/GridSearch.js
@@ -189,8 +189,10 @@ define([
 
       var searchValue = evt.target.value;
 
-      delay(lang.hitch(this, function () {
+      delay(lang.hitch(this, function() {
+        this._actorGrid.set('_started', false); // Avoid unnecessary rendering and processing
         this._removeSort(); // remove sort when searching (for ES)
+        this._actorGrid.set('_started', true);
         if (searchValue && searchValue !== '') {
           this._actorGrid.set('collection', this.actorStore.filter({omschrijving: searchValue}));
         } else {
